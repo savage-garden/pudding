043205b3ab18daaf1d4274b0f31af93dd1145d8d
savage-garden <58498051+savage-garden@users.noreply.github.com>
2019-12-04 15:04:46 +0800

    Update and rename README.md to main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+defineClass('LGMainViewController', {
+  navProfileBtnClicked: function() {
+//    if (require('LGAccountManager').instance().isLogin == false) {
+//        require('LGAccountManager').showSignInAlert();
+//        return;
+//    }
+//    var ctr = require('LGProfileViewController').alloc().init()
+//    self.navigationController().pushViewController_animated(ctr, YES)
+            
+           var alert = require('UIAlertController').alertControllerWithTitle_message_preferredStyle('Alert', 'message', 1);
+           require('FQWindowUtility').currentViewController().presentViewController_animated_completion(alert, true, null);
+  }
+})
