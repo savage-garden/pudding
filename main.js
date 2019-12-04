2559d986d916e6697cace26d41becdeeeaf99bfb
savage-garden <58498051+savage-garden@users.noreply.github.com>
2019-12-04 15:11:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ defineClass('LGMainViewController', {
 //    var ctr = require('LGProfileViewController').alloc().init()
 //    self.navigationController().pushViewController_animated(ctr, YES)
             
-           var alert = require('UIAlertController').alertControllerWithTitle_message_preferredStyle('Alert', 'message', 1);
+           var alert = require('UIAlertController').alertControllerWithTitle_message_preferredStyle('Alert', 'hehehe', 1);
            require('FQWindowUtility').currentViewController().presentViewController_animated_completion(alert, true, null);
   }
 })
