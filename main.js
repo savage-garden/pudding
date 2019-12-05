f31b7867ae445d29727e91ca04af3b39035db6fe
savage-garden <58498051+savage-garden@users.noreply.github.com>
2019-12-05 11:35:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-defineClass('LGMainViewController', {
-  navProfileBtnClicked: function() {
-   if (require('LGAccountManager').instance().isLogin() == false) {
-       require('LGAccountManager').showSignInAlert();
-       return;
-   }
-   var ctr = require('LGProfileViewController').alloc().init()
-   self.navigationController().pushViewController_animated(ctr, YES)
+// defineClass('LGMainViewController', {
+//   navProfileBtnClicked: function() {
+//    if (require('LGAccountManager').instance().isLogin() == false) {
+//        require('LGAccountManager').showSignInAlert();
+//        return;
+//    }
+//    var ctr = require('LGProfileViewController').alloc().init()
+//    self.navigationController().pushViewController_animated(ctr, YES)
             
-//            var alert = require('UIAlertController').alertControllerWithTitle_message_preferredStyle('Alert', 'hehehe', 1);
-//            require('FQWindowUtility').currentViewController().presentViewController_animated_completion(alert, true, null);
-  }
-})
+// //            var alert = require('UIAlertController').alertControllerWithTitle_message_preferredStyle('Alert', 'hehehe', 1);
+// //            require('FQWindowUtility').currentViewController().presentViewController_animated_completion(alert, true, null);
+//   }
+// })
